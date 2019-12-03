f0ab84c7f7d8da5591889ba5659a2224dbf612b1
magicalmeghan <magicalmeghan@google.com>
2019-12-03 11:58:18 -0800

    starter code

--- app/src/main/java/com/example/android/treasureHunt/GeofenceBroadcastReceiver.kt
+++ app/src/main/java/com/example/android/treasureHunt/GeofenceBroadcastReceiver.kt
@@ -37,48 +37,7 @@ import com.google.android.gms.location.GeofencingEvent
 class GeofenceBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == ACTION_GEOFENCE_EVENT) {
-            val geofencingEvent = GeofencingEvent.fromIntent(intent)
-
-            if (geofencingEvent.hasError()) {
-                val errorMessage = errorMessage(context, geofencingEvent.errorCode)
-                Log.e(TAG, errorMessage)
-                return
-            }
-
-            if (geofencingEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
-                Log.v(TAG, context.getString(R.string.geofence_entered))
-
-                val fenceId = when {
-                    geofencingEvent.triggeringGeofences.isNotEmpty() ->
-                        geofencingEvent.triggeringGeofences[0].requestId
-                    else -> {
-                        Log.e(TAG, "No Geofence Trigger Found! Abort mission!")
-                        return
-                    }
-                }
-                // Check geofence against the constants listed in GeofenceUtil.kt to see if the
-                // user has entered any of the locations we track for geofences.
-                val foundIndex = GeofencingConstants.LANDMARK_DATA.indexOfFirst {
-                    it.id == fenceId
-                }
-
-                // Unknown Geofences aren't helpful to us
-                if ( -1 == foundIndex ) {
-                    Log.e(TAG, "Unknown Geofence: Abort Mission")
-                    return
-                }
-
-                val notificationManager = ContextCompat.getSystemService(
-                    context,
-                    NotificationManager::class.java
-                ) as NotificationManager
-
-                notificationManager.sendGeofenceEnteredNotification(
-                    context, foundIndex
-                )
-            }
-        }
+        // TODO: Step 11 implement the onReceive method
     }
 }
 
